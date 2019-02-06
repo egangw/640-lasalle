d684a89de60fbba3db004f3c2ea4cbf57dc3f861
Erika Gangware <erikagangware@gmail.com>
2019-02-05 19:04:51 -0600

    Stereo VR to monoscopic screen toggle function

---
+++ 640-lasalle/Assets/VRToggle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class VRToggle : MonoBehaviour {
+
+
+		void Update() {
+
+			if (Input.GetMouseButtonDown (0)) {
+				ToggleVR ();
+			}
+		}
+
+		void ToggleVR() {
+
+			if (XRSettings.loadedDeviceName == "cardboard") {
+				StartCoroutine(LoadDevice("None"));
+			} else {
+				StartCoroutine(LoadDevice("cardboard"));
+			}
+		}
+
+		IEnumerator LoadDevice(string newDevice)
+		{
+			XRSettings.LoadDeviceByName(newDevice);
+			yield return null;
+			XRSettings.enabled = true;
+		}
+	}
---
+++ 640-lasalle/Assets/monoMode.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using Gvr;
+
+public class monoMode : MonoBehaviour {
+
+	//public bool setStereoMode;
+
+		//public void setStereoModeEnabled () {
+
+		//XRSettings.enabled = false;
+		//Camera.main.ResetAspect ();
+		//Camera.main.Render = InputTracking.GetLocalRotation(XRNode.CenterEye);
+		//setStereoMode = true;
+
+	//}
+	// Use this for initialization
+	void Start () {
+
+		XRSettings.enabled = true;
+		StartCoroutine(LoadDevice ("cardboard", false));
+		Camera.main.ResetAspect ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+	IEnumerator LoadDevice(string newDevice, bool enable)
+	{
+		XRSettings.LoadDeviceByName(newDevice);
+		yield return null;
+		XRSettings.enabled = enable;
+	}
+
+	void EnableVR()
+	{
+		StartCoroutine(LoadDevice("cardboard", true));
+	}
+
+	void DisableVR()
+	{
+		StartCoroutine(LoadDevice("", false));
+	}
+
+}
